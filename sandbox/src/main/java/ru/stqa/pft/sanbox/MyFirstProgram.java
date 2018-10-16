cb0937ffb867183114e94ad0c5b82b0b5b9be5f6
John Heifetz <mac_heif@Mac-Heif.local>
2018-10-16 17:54:27 +0300

    Homework #2 complete

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -12,9 +12,16 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+		System.out.println();
+
+		//hometask #2
+		Point p1 = new Point( 12, 12);
+		Point p2 = new Point(9,9);
+		System.out.println("Расстояние между двумя точками на плоскости с координатами" + " x1 = " + p1.x + ", y1 = " + p1.y + " и " + " x2 = " + p2.x + ", y2 = " + p2.y  + " равно " + p1.distance(p2));
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello, " + somebody + "!");
 	}
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sanbox;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p2) {
+      return Math.sqrt((Math.pow(p2.x - this.x, 2)) + (Math.pow(p2.y - this.y, 2)));
+    }
+}
