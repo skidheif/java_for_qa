dbc6d9dde87d15b472f0195cf8a2f20b848f812a
Andrey <skidheif@gmail.com>
2018-10-28 18:35:59 +0300

    add code from lesson 3.3-3.4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,21 +1,35 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        String browser = BrowserType.FIREFOX;
-        wd = new FirefoxDriver();
+//        if (browser.equals(BrowserType.FIREFOX)){
+//            wd = new FirefoxDriver();
+//        } else if (browser.equals(BrowserType.CHROME)){
+//            wd = new ChromeDriver();
+//        } else if (browser.equals(BrowserType.SAFARI)){
+//            wd = new SafariDriver();
+//        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
         sessionHelper = new SessionHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -8,16 +9,16 @@ import ru.stqa.pft.addressbook.model.ContactData;
 
 public class TestBase{
 
-    protected final ApplicationManager app = new ApplicationManager();
-
-    @BeforeMethod
-    public void setUp(){
-        app.init();
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        app.stop();
-    }
+//    protected final ApplicationManager app = new ApplicationManager(BrowserType.SAFARI);
+//
+//    @BeforeMethod
+//    public void setUp(){
+//        app.init();
+//    }
+//
+//    @AfterMethod
+//    public void tearDown(){
+//        app.stop();
+//    }
 
 }
