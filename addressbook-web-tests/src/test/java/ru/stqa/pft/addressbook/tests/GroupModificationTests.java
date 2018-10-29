059fdcd4bacfb1e07c418876125a84edba5f5c34
John Heifetz <mac_heif@Mac-Heif.local>
2018-10-29 17:01:49 +0300

    add code from lessons 3.5-3.6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -11,7 +11,7 @@ public class GroupModificationTests extends TestBase{
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("aaaa", "bbbb", "cccc"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
