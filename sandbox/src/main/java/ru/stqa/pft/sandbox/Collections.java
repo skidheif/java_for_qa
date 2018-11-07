0ade03277989e0ce1e887701082e889d4f06c07e
John Heifetz <mac_heif@Mac-Heif-W.local>
2018-11-07 18:13:37 +0300

    add some code

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        String [] langs = new String[4];
+        langs[0] = "Java";
+        langs[1] = "C#";
+        langs[2] = "Python";
+        langs[3] = "PHP";
+    }
+}
