94aea9dce6912a07e4d56adcd0ca1db9783335d8
Andrey <skidheif@gmail.com>
2018-11-11 12:12:30 +0300

    add code from lesson 4.2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,12 +1,17 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
     public static void main(String[] args) {
-        String [] langs = new String[4];
-        langs[0] = "Java";
-        langs[1] = "C#";
-        langs[2] = "Python";
-        langs[3] = "PHP";
+        String[] langs = {"Java", "C#", "Python", "PHP"};
+
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+        for (String l : languages) {
+            System.out.println("Я хочу выучить " + l);
+        }
     }
 }
