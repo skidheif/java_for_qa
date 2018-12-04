ee0c987ce10f05323137f5555fcb2b60779e6d8b
John Heifetz <mac_heif@Mac-Heif-Work.local>
2018-12-04 11:40:44 +0300

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -25,7 +25,7 @@ public class ApplicationManager {
 
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
-        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        properties.load(new FileReader(new File(String.format("src/main/resources/%s.properties", target))));
 
 
         if (browser.equals(BrowserType.FIREFOX)) {
@@ -44,4 +44,11 @@ public class ApplicationManager {
         wd.quit();
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
