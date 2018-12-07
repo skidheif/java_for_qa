0edecfc5a0e859dd0e815cf9602007d5c1e31ef3
John Heifetz <mac_heif@Mac-Heif-Work.local>
2018-12-07 17:20:49 +0300

    change path in two projects

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
-        app.ftp().upload(new File("src/main/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
