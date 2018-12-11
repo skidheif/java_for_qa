1efcf353660be4053b8e0cc92613901a3c0e1e71
John Heifetz <mac_heif@Mac-Heif-Work.local>
2018-12-11 15:24:41 +0300

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("c6e2cbbf9b99290422097810fd4e22da3b180eb5");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("skidheif", "java_for_qa")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
